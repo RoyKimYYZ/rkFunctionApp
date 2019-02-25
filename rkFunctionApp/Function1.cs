d7034294b3d0195a57ea190f8845d1510df59150
Roy Kim (MVP) <rsx79@hotmail.com>
2019-02-25 18:27:56 -0500

    initial

--- rkFunctionApp/Function1.cs
+++ rkFunctionApp/Function1.cs
@@ -5,12 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace rkFunctionApp
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("GetRecipes")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
@@ -27,9 +32,46 @@ namespace rkFunctionApp
                 name = data?.name;
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            string content = String.Empty;
+            ArrayList list = new ArrayList();
+            bool hasRows;
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("DatabaseConnectionString");
+            log.Info(String.Format("{0} connection string", str));
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "SELECT * FROM recipes";
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    SqlDataReader rows = await cmd.ExecuteReaderAsync();
+                    log.Info($"{rows.HasRows} has rows");
+
+                    if (rows.HasRows)
+                    {
+                        while (rows.Read())
+                        {
+                            log.Info($"{rows.GetInt32(0)}\t{rows.GetString(1)}");
+                            list.Add(rows.GetString(1));
+                        }
+                    }
+                    else
+                    {
+                        log.Info($"No rows found.");
+                    }
+
+                    hasRows = rows.HasRows;
+                    content = JsonConvert.SerializeObject(list);
+
+                }
+            }
+            
+            return !hasRows
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Bad request")
+                : req.CreateResponse(HttpStatusCode.OK, content, "application/json");
         }
     }
 }
